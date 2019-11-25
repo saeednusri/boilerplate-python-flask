1437ba091549626b1be59b047576752e67368991
Saeed Nusri <saeednusri@M0190136000.local>
2019-11-25 14:04:55 -0700

    refactored for pytest watch

--- myapp/package/functions.py
+++ myapp/package/functions.py
@@ -3,3 +3,4 @@ import os
 def add(x: int, y: int) -> int:
     z = x + y 
     return z
+
--- myapp/package/test_functions.py
+++ myapp/package/test_functions.py
@@ -1,9 +1,12 @@
 import unittest
 import functions 
 
+# Add test
 class test_package(unittest.TestCase):
     def test_add(self):
         assert functions.add(0,1) == 1
+
+
         
         
 
